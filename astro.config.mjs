7cdd53645ef10da408fc05d316a898c6aa3879e0
Nikolay Pugyov <n.d.pugyov@gmail.com>
2026-06-17 13:50:37 +0300

    Use non-www canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://www.dimtex.bg",
+  site: "https://dimtex.bg",
   output: "static",
   integrations: [tailwind(), react(), sitemap()]
 });
